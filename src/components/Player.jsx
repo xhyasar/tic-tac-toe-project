2e878bb3522321c60aea702cee437d1d8f99357b
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-12 15:02:02 +0300

    add edit functionality to Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,18 @@
+import {useState} from 'react';
+
+
 export default function Player({name, symbol}){
+    const [isEditing, setIsEditing] = useState(false);
+    function handleEditClick(){
+        setIsEditing(true);
+
+    }
     return( <li>
           <span className="player">
           <span className="player-name" >{name}</span>
           <span className="player-symbol">{symbol}</span>
           </span>
-        <button>Edit</button>
+        <button onClick={handleEditClick}>Edit</button>
     </li>
     );
 }
