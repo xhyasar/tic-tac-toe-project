7e483e53cb919b3b551683e079efbfd28c406f33
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-12 15:18:35 +0300

    toggle edit mode and update button caption in Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,18 +4,21 @@ import {useState} from 'react';
 export default function Player({name, symbol}){
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick(){
-        setIsEditing(true);
+        setIsEditing(!isEditing);
     }
     let playerName= <span className="player-name" >{name}</span>;
+    // let btnCaption = 'Edit';
+
     if (isEditing){
-        playerName=<input type="text" required/>
+        playerName=<input type="text" required value ={name}/>;
+        //btnCaption='Save';
     }
     return( <li>
           <span className="player">
               {playerName}
           <span className="player-symbol">{symbol}</span>
           </span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
     );
 }
