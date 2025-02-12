cf789855437fcc46262546c0b59ae2df98ca9084
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-12 19:36:21 +0300

    add GameBoard component and update Player component to support editable names

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
 
 function App() {
 
   return <main>
     <div id="game-container">
       <ol id="players">
-        <Player name="Player 1" symbol="X"/>
-        <Player name="Player 2" symbol="O"/>
+        <Player initiaName="Player 1" symbol="X"/>
+        <Player initiaName="Player 2" symbol="O"/>
       </ol>
-      GAME BOARD
+      <GameBoard/>
     </div>
 
     LOG
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,27 @@
 import {useState} from 'react';
 
 
-export default function Player({name, symbol}){
+export default function Player({initiaName, symbol}){
+    const [playerName, setPlayerName] = useState(initiaName);
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick(){
         setIsEditing((editing) => !editing);
     }
-    let playerName= <span className="player-name" >{name}</span>;
+
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName= <span className="player-name" >{playerName}</span>;
     // let btnCaption = 'Edit';
 
     if (isEditing){
-        playerName=<input type="text" required value ={name}/>;
+        editablePlayerName=<input type="text" required value ={playerName} onChange={handleChange}/>;
         //btnCaption='Save';
     }
     return( <li>
           <span className="player">
-              {playerName}
+              {editablePlayerName}
           <span className="player-symbol">{symbol}</span>
           </span>
         <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
