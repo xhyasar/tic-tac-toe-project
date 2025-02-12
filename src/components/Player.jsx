f2d1a3e3b53fbaf478aefd9335ad4578666f20fa
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-12 15:04:57 +0300

    add editing capability to player name in Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,11 +5,14 @@ export default function Player({name, symbol}){
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick(){
         setIsEditing(true);
-
+    }
+    let playerName= <span className="player-name" >{name}</span>;
+    if (isEditing){
+        playerName=<input type="text" required/>
     }
     return( <li>
           <span className="player">
-          <span className="player-name" >{name}</span>
+              {playerName}
           <span className="player-symbol">{symbol}</span>
           </span>
         <button onClick={handleEditClick}>Edit</button>
