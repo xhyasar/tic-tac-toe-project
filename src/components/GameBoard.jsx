ddd506331be08a74c2d8927f6506d418fbe29174
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-12 20:06:16 +0300

    update GameBoard component to handle square selection and state updates

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -8,17 +8,19 @@ const initialGameBoard =[
 
 export default function GameBoard() {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
         function handleSelectSquare(rowIndex,colIndex){
             setGameBoard((prevGameBoard) => {
-                prevGameBoard[rowIndex,colIndex] = "X";
-                return prevGameBoard;
+                const updatedBoard =[...prevGameBoard.map (innerArray => [...innerArray])];
+                updatedBoard[rowIndex][colIndex] = "X";
+                return updatedBoard;
             });
         }
 
     return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex)=> <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex)=> <li key={rowIndex}>
             <ol>
-                {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
+                {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
             </ol>
         </li>)
         }
