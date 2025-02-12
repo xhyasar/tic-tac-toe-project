addb0c4e12d33c4eede6560e7262c3a49568acc3
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-12 15:28:02 +0300

    refactor edit toggle logic in Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ import {useState} from 'react';
 export default function Player({name, symbol}){
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick(){
-        setIsEditing(!isEditing);
+        setIsEditing((editing) => !editing);
     }
     let playerName= <span className="player-name" >{name}</span>;
     // let btnCaption = 'Edit';
