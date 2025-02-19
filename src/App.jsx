aec5a3a38c4869bd08fdb75bc59fbe344744a42c
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-19 17:41:33 +0300

    refactor active player management logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,28 @@ import GameBoard from './components/GameBoard.jsx';
 import {useState} from "react";
 import Log from "./components/Log.jsx";
 
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns,setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  //const [activePlayer, setActivePlayer] = useState('X');
+const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+   // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns =>{
-      let currentPlayer = 'X';
-      if (prevTurns.length >0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O';
-      }
-
-      return[
-          { square: {row : rowIndex, col: colIndex}, player: currentPlayer},
-        ...prevTurns,
-      ];
+      const currentPlayer =deriveActivePlayer(prevTurns);
+const updatedTurns = [
+  { square: {row : rowIndex, col: colIndex}, player: currentPlayer},
+  ...prevTurns,
+]
+      return updatedTurns;
     })
   }
 
