9f1e066ee1fe89660c37946d3c92ade4d30b28bc
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-19 19:19:31 +0300

    add draw condition and update GameOver component to handle draws

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ for (const combination of WINNING_COMBINATIONS){
     winner = firstSquareSymbol;
   }
 }
+const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
@@ -66,7 +67,7 @@ const updatedTurns = [
             <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
             <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
           </ol>
-          {winner && <GameOver winner={winner} />}
+          {(winner || hasDraw) && <GameOver winner={winner} />}
           <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
         <Log turns={gameTurns}/>
